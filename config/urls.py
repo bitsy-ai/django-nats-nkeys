b1aba9ea46082ee84e4f6d2eec4aff8d67f1cc4e
Leigh Johnson <leigh@bitsy.ai>
2022-07-30 21:11:29 -0700

    lint

--- config/urls.py
+++ config/urls.py
@@ -4,5 +4,5 @@ from django.contrib import admin
 urlpatterns = [
     path("admin/", admin.site.urls),
     # path("coturn/", include("django_coturn.urls")),
-    path('accounts/', include("django.contrib.auth.urls")),
+    path("accounts/", include("django.contrib.auth.urls")),
 ]
