5ec6f3aa28cb6c649d0f0ec3c1972cd5fe679a6d
Leigh Johnson <leigh@bitsy.ai>
2022-08-14 13:18:12 -0700

    Django management command: nsc_operator_jwt (#13)
    * implement nsc_operator_jwt management command
    
    * add --force
    
    * fix action
    
    * fix flag

---
+++ django_nats_nkeys/management/commands/nsc_operator_jwt.py
@@ -0,0 +1,54 @@
+from multiprocessing.sharedctypes import Value
+from click import BaseCommand
+from django.core.management.base import BaseCommand, CommandParser
+
+from django_nats_nkeys.settings import nats_nkeys_settings
+from django_nats_nkeys.services import run_nsc_and_log_output
+
+
+class Command(BaseCommand):
+    def add_arguments(self, parser: CommandParser) -> None:
+
+        parser.add_argument(
+            "--import",
+            type=str,
+            help="Import operator from operator.jwt",
+        )
+        parser.add_argument(
+            "--export",
+            type=str,
+            help="Create operator.jwt",
+        )
+        parser.add_argument(
+            "--name",
+            type=str,
+            help="Operator name",
+            default=nats_nkeys_settings.NATS_NKEYS_OPERATOR_NAME,
+        )
+        parser.add_argument("--force", action="store_true", default=False)
+
+    def handle(self, *args, **kwargs):
+        importf = kwargs.get("import")
+        exportf = kwargs.get("export")
+        name = kwargs.get("name")
+        force = kwargs.get("force")
+        if importf is None and exportf is None:
+            raise ValueError("One of --import|export is required")
+        if importf is not None and exportf is not None:
+            raise ValueError("Please specify one of --import|export (received both)")
+
+        if importf is not None:
+            cmd = ["nsc", "add", "operator", "-u", importf]
+            if force is True:
+                cmd += ["--force"]
+            run_nsc_and_log_output(cmd)
+            self.stdout.write(
+                self.style.SUCCESS(f"Success! Imported from JWT {importf}")
+            )
+        if exportf is not None:
+            run_nsc_and_log_output(
+                ["nsc", "describe", "operator", "--raw", "--output-file", exportf]
+            )
+            self.stdout.write(
+                self.style.SUCCESS(f"Success! Wrote operator JWT to {importf}")
+            )
--- tests/test_command.py
+++ tests/test_command.py
@@ -1,3 +1,4 @@
+import tempfile
 from django.contrib.auth import get_user_model
 from django.core.management import call_command
 from django.test import TestCase
@@ -15,9 +16,17 @@ class TestCommand(TestCase):
 
     def test_nsc_push(self):
         # test non-zero exits
-        call_command("nsc_push")
         call_command("nsc_push", "--force")
+        call_command("nsc_push")
 
     def test_nsc_pull(self):
-        call_command("nsc_pull")
         call_command("nsc_pull", "--force")
+        call_command("nsc_pull")
+
+    def test_nsc_operator_jwt(self):
+
+        with tempfile.NamedTemporaryFile() as f:
+            filename = f.name
+        # exit context and delete file. nsc doesn't have an --overwrite flag for nsc describe... command will fail if file exists
+        call_command("nsc_operator_jwt", "--export", f"{f.name}")
+        call_command("nsc_operator_jwt", "--import", f"{f.name}", "--force")
