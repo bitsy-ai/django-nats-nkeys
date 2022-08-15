a25c0352624ce6ab12cf509c3e59555d9cbb3b38
Leigh Johnson <leigh@bitsy.ai>
2022-08-15 11:21:49 -0700

    implement import/export management commands for user/account jwts

--- django_nats_nkeys/management/commands/nsc_user_jwt.py
+++ django_nats_nkeys/management/commands/nsc_user_jwt.py
@@ -2,7 +2,6 @@ from multiprocessing.sharedctypes import Value
 from click import BaseCommand
 from django.core.management.base import BaseCommand, CommandParser
 
-from django_nats_nkeys.settings import nats_nkeys_settings
 from django_nats_nkeys.services import run_nsc_and_log_output
 
 
@@ -12,36 +11,54 @@ class Command(BaseCommand):
         parser.add_argument(
             "--import",
             type=str,
-            help="Import operator from operator.jwt",
+            help="Import user from .jwt",
+            required=True,
         )
         parser.add_argument(
             "--export",
             type=str,
             help="Create operator.jwt",
         )
+        parser.add_argument(
+            "--name",
+            type=str,
+            help="Account name",
+        )
         parser.add_argument("--force", action="store_true", default=False)
 
     def handle(self, *args, **kwargs):
         importf = kwargs.get("import")
-        exportf = kwargs.get("export")
         force = kwargs.get("force")
+        exportf = kwargs.get("export")
+        name = kwargs.get("name")
+
         if importf is None and exportf is None:
             raise ValueError("One of --import|export is required")
         if importf is not None and exportf is not None:
             raise ValueError("Please specify one of --import|export (received both)")
+        if exportf is not None and name is None:
+            raise ValueError("--name is required")
 
         if importf is not None:
-            cmd = ["nsc", "add", "operator", "-u", importf]
+            cmd = ["nsc", "import", "user", "--file", importf]
             if force is True:
                 cmd += ["--force"]
             run_nsc_and_log_output(cmd)
             self.stdout.write(
                 self.style.SUCCESS(f"Success! Imported from JWT {importf}")
             )
+
         if exportf is not None:
-            run_nsc_and_log_output(
-                ["nsc", "describe", "operator", "--raw", "--output-file", exportf]
-            )
+            cmd = [
+                "nsc",
+                "describe",
+                "user",
+                "--name",
+                name,
+                "--raw",
+                "--output-file",
+                exportf,
+            ]
             self.stdout.write(
-                self.style.SUCCESS(f"Success! Wrote operator JWT to {importf}")
+                self.style.SUCCESS(f"Success! Output user JWT to {exportf}")
             )
