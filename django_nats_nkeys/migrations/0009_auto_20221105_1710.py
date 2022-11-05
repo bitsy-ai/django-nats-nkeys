ec195def7da081c2686b4c937376bce255c273fa
Leigh Johnson <leigh@bitsy.ai>
2022-11-05 10:12:01 -0700

    lint

--- django_nats_nkeys/migrations/0009_auto_20221105_1710.py
+++ django_nats_nkeys/migrations/0009_auto_20221105_1710.py
@@ -7,18 +7,26 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('django_nats_nkeys', '0008_auto_20221105_0256'),
+        ("django_nats_nkeys", "0008_auto_20221105_0256"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='natsorganizationapp',
-            name='organization',
-            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='nats_organization_apps', to='django_nats_nkeys.natsorganization'),
+            model_name="natsorganizationapp",
+            name="organization",
+            field=models.ForeignKey(
+                on_delete=django.db.models.deletion.CASCADE,
+                related_name="nats_organization_apps",
+                to="django_nats_nkeys.natsorganization",
+            ),
         ),
         migrations.AlterField(
-            model_name='natsorganizationapp',
-            name='organization_user',
-            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='nats_organization_apps', to='django_nats_nkeys.natsorganizationuser'),
+            model_name="natsorganizationapp",
+            name="organization_user",
+            field=models.ForeignKey(
+                on_delete=django.db.models.deletion.CASCADE,
+                related_name="nats_organization_apps",
+                to="django_nats_nkeys.natsorganizationuser",
+            ),
         ),
     ]
